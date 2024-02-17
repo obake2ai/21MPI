99bdf7ad414c9b18841052ec2687dc815e77d54d
kishi yuma <fableooon@gmail.com>
2024-02-17 22:34:55 +0900

    Update 75_sync_image_21m.py

--- 75_sync_image_21m.py
+++ 75_sync_image_21m.py
@@ -21,7 +21,7 @@ def get_newest_file(path):
     return os.path.join(path, newest_file), os.path.getmtime(os.path.join(path, newest_file))
 
 def display_image(file_path):
-    subprocess.run(['feh', '--fullscreen', '--reload', '1', file_path])
+    subprocess.run(['feh', '--fullscreen', '--auto-rotate', '--zoom', 'fill', file_path])
 
 def main():
     last_update_time = None
@@ -34,7 +34,7 @@ def main():
             last_update_time = file_time
             if feh_process:
                 feh_process.kill()
-            feh_process = subprocess.Popen(['feh', '--fullscreen', newest_file])
+            feh_process = subprocess.Popen(['feh', '--fullscreen', '--auto-rotate', '--zoom', 'fill', newest_file])
         time.sleep(10)
 
 if __name__ == "__main__":
