d769a5752809c91686239a55fab429f3dbe37f9d
Yuma Kishi <obake2ai@gmail.com>
2024-09-20 15:31:01 +0900

    update

---
+++ play_loop_video.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+
+os.environ['DISPLAY'] = ':0'
+
+if len(sys.argv) > 1:
+    video_file = sys.argv[1]
+else:
+    raise ValueError("No video file specified.")
+
+def play_video_loop(video_file_path):
+    # omxplayerでループ再生
+    subprocess.run(['omxplayer', '--loop', '--no-osd', '--aspect-mode', 'fill', video_file_path])
+
+def main():
+    if not os.path.isfile(video_file):
+        raise FileNotFoundError(f"{video_file} not found.")
+
+    play_video_loop(video_file)
+
+if __name__ == "__main__":
+    main()
